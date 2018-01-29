f96fb9d92c512569a4e9402660421b139c8a029e
Jack McGillick <jackmcgillick@windstream.net>
2018-01-28 19:19:20 -0500

    Added README.txt and set up pygame with rgb.py

--- rgb.py
+++ rgb.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
 
 import serial
+import pygame, sys
+from pygame.locals import *
 
 s = serial.Serial("/dev/ttyACM0")
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400,300))
+pygame.display.set_caption('RGB Test')
 
-while(True)
+while(True):
    l = s.readline()
    x = l.rstrip().split(",")
    rgb = [int(val) for val in x]
    print x
+   #pygame.Color(rgb)
+   pygame.display.update()
